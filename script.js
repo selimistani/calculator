6fc5d86175d73c60c6657727f4f4aae68be50d53
selimistani <142060673+selimistani@users.noreply.github.com>
2024-09-09 17:12:46 +0400

    Implement functionality to populate the display with number button clicks

--- script.js
+++ script.js
@@ -28,3 +28,17 @@ function operate(num1, num2, operator) {
       return "Invalid operator";
   }
 }
+
+// Populating Display with numbers
+
+let displayValue = "";
+const numberButtons = document.querySelectorAll(".calculator-btns button");
+const displayDiv = document.querySelector("#output");
+const previousDisplayDiv = document.querySelector("#output-previous");
+
+numberButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    displayValue += String(button.id);
+    displayDiv.textContent = displayValue;
+  })
+);

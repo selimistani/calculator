47273e35638114d50d1d03df8c99b96436f0d5ce
selimistani <142060673+selimistani@users.noreply.github.com>
2024-09-09 17:33:37 +0400

    Add logic to handle operations and calculate results when equals button is pressed

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ function divide(num1, num2) {
 }
 
 function operate(num1, num2, operator) {
+  num1 = Number(num1);
+  num2 = Number(num2);
   switch (operator) {
     case "+":
       return add(num1, num2);
@@ -32,13 +34,37 @@ function operate(num1, num2, operator) {
 // Populating Display with numbers
 
 let displayValue = "";
-const numberButtons = document.querySelectorAll(".calculator-btns button");
+let operator = "";
+const buttons = document.querySelectorAll(".calculator-btns button");
 const displayDiv = document.querySelector("#output");
 const previousDisplayDiv = document.querySelector("#output-previous");
+let num1 = NaN,
+  num2 = NaN;
 
-numberButtons.forEach((button) =>
-  button.addEventListener("click", () => {
-    displayValue += String(button.id);
-    displayDiv.textContent = displayValue;
-  })
-);
+buttons.forEach((button) => {
+  if (button.classList.contains("number")) {
+    button.addEventListener("click", () => {
+      displayValue += button.id;
+      displayDiv.textContent = displayValue;
+    });
+  } else if (button.classList.contains("operator")) {
+    button.addEventListener("click", () => {
+      if (!num1) {
+        num1 = displayValue;
+        previousDisplayDiv.textContent = num1;
+        displayValue = "";
+        displayDiv.textContent = displayValue;
+        operator = button.value;
+      }
+    });
+  } else if (button.id === "=") {
+    button.addEventListener("click", () => {
+      if (num1 && operator) {
+        previousDisplayDiv.textContent = "";
+        num2 = displayValue;
+        displayValue = operate(num1, num2, operator);
+        displayDiv.textContent = displayValue;
+      }
+    });
+  }
+});
